7128cee4e0655dc9e92f1c3b16990aadecd8e9a9
Imad Kazi <imad@Imads-MacBook-Pro.local>
2021-10-08 11:22:42 +0100

    Craco add and update

--- Tailwind/tailwind/src/App.js
+++ Tailwind/tailwind/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>
+        <h2 className="bg-red-500 px-4 py-2 m-2 rounded-md">
           <b>Hello World</b>
         </h2>
         <p>
