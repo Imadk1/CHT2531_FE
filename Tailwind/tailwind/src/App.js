ca225adc6e325651a401f51bce683af252302f5b
Imad Kazi <imad@Imads-MacBook-Pro.local>
2021-10-08 11:30:51 +0100

    Components

--- Tailwind/tailwind/src/App.js
+++ Tailwind/tailwind/src/App.js
@@ -1,26 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2 className="bg-red-500 px-4 py-2 m-2 rounded-md">
           <b>Hello World</b>
         </h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
